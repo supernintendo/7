ebc8cdcb8bf5d993aa6e6880a625f79e561ab52d
Michael Matyi <michaelmatyi@icloud.com>
2015-05-18 12:53:58 -0700

    shop > Build script.

---
+++ shop/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+build_frontend()
+{
+  cd public && npm install && npm run build
+}
+
+build_server()
+{
+  bundler_present="$(gem query --name-matches 'bundler' --installed)"
+  if [ $bundler_present == "false" ]
+  then
+    gem install bundler
+  fi
+  bundle install
+}
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -b|--build)
+    BUILD="$2"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
+
+if [ -z "$BUILD" ]
+then
+  build_frontend
+  build_server
+else
+  if [ $BUILD == "frontend" ]
+  then
+    build_frontend
+  elif [ $BUILD == "server" ]
+  then
+    build_server
+  else
+    echo "ERROR: No build task for: ${BUILD}"
+  fi
+fi
