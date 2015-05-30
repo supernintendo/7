4d3779daf6f9aa30d2f941ebef5f675486bca2ea
Michael Matyi <michaelmatyi@icloud.com>
2015-05-30 16:02:32 -0700

    time > Change colors based on time of day.

--- time/app.js
+++ time/app.js
@@ -1,5 +1,6 @@
 ~function() {
     var inner = document.getElementById('inner-container'),
+        outer = document.getElementById('outer-container'),
         timer = document.getElementById('time'),
         addClass = function(el, className) {
             if (el.classList) {
@@ -15,7 +16,32 @@
             }, 1);
         },
         getTimeRange = function() {
-            console.log(moment().format('h'));
+            var hour = Number(moment().format('H'));
+
+            if (hour >= 6 && hour < 12) {
+                return 'dawn';
+            } else if (hour >= 12 && hour < 17) {
+                return 'day';
+            } else if (hour >= 17 && hour < 20) {
+                return 'dusk';
+            } else {
+                return 'night';
+            }
+        },
+        isBackgroundClass = function(className) {
+            return className.indexOf('dawn') > -1 ||
+                   className.indexOf('day') > -1 ||
+                   className.indexOf('dusk') > -1 ||
+                   className.indexOf('night') > -1;
+        },
+        removeBackgroundClass = function() {
+            var classes = outer.classList, i;
+
+            for (i = 0; i < classes.length; i++) {
+                if (isBackgroundClass(classes[i])) {
+                    removeClass(outer, classes[i]);
+                }
+            }
         },
         removeClass = function(el, className) {
             if (el.classList) {
@@ -23,6 +49,19 @@
             } else {
                 el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
             }
+        },
+        shuffleBackground = function() {
+            var n = Math.floor((Math.random() * 4) + 1);
+
+            removeBackgroundClass();
+            addClass(outer, getTimeRange() + n);
+
+            if (getTimeRange() === 'dusk' || getTimeRange() === 'night') {
+                addClass(timer, 'invert');
+            } else {
+                removeClass(timer, 'invert');
+            }
+            fadeInner();
         },
         updateTimer = function() {
             var parts = [
@@ -38,7 +77,7 @@
         };
 
     updateTimer();
-    getTimeRange();
-    fadeInner();
+    shuffleBackground();
     setInterval(updateTimer, 1000);
+    setInterval(shuffleBackground, 30000);
 }();
