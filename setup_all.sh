49003d14af170b200c910cbc0d641061167443b9
Michael Matyi <michaelmatyi@icloud.com>
2015-07-02 00:15:31 -0700

    master > update setup_all script

--- setup_all.sh
+++ setup_all.sh
@@ -2,7 +2,7 @@
 
 cd pixel && npm install && npm run build && cd ..
 cd scrapbook/server && bundle install && cd .. && npm install && cd ..
-cd shop && bundle install && cd public && npm install && npm run build && cd ../..
+cd shop/server && bundle install && cd public && npm install && npm run build && cd ../../..
 cd time && npm install && cd ..
 cd todo && npm install && npm run build && cd ..
 cd wtf && npm install && npm run build && cd ..
